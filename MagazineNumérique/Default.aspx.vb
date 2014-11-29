b5a30191a9dd1d4702167b725c606831bf2c9c26
msknmehdi <msknmehdi@gmail.com>
2014-11-29 16:36:27 +0100

    Magazine numérique
    Commit initial :
    - Page d'acceuil
    - Template Geass.

---
+++ MagazineNumérique/Default.aspx.vb
@@ -0,0 +1,10 @@
+﻿
+Partial Class _Default
+    Inherits System.Web.UI.Page
+
+
+
+    Protected Sub Page_Load(sender As Object, e As EventArgs) Handles Me.Load
+        Response.Redirect("./index.aspx")
+    End Sub
+End Class
